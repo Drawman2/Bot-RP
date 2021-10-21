7a37c8a04c74d4e8d38544aafd6a3cb96a16ea4b
Drawman2 <drawmant@gmail.com>
2021-10-21 18:20:04 +0200

    Fixed secrets on Windows because .env was not loaded

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import discord
 from discord.ext import commands
 
 import os
+import dotenv
+
+#On windows only:
+dotenv.load_dotenv()
 
 client = commands.Bot(command_prefix = "/")
 
@@ -12,8 +16,6 @@ async def on_ready():
     await client.change_presence(activity=discord.Game(name="/help"))
     print("Bot online and running...")
 
-client.run(os.getenv("BOT_TOKEN"))
-
 @client.command()
 
 #The name of each function is the same as the command you want to have
@@ -22,4 +24,7 @@ client.run(os.getenv("BOT_TOKEN"))
 #contains informations about it and allows to send things on Discord.
 async def ping(ctx):
     #latency is multiplied by 1000 because it was originally in seconds.
-    await ctx.send("Pong! {}ms".format(round(client.lantency * 1000)))
+    await ctx.send("Pong! {}ms".format(round(client.latency * 1000)))
+
+#This needs to be at the end
+client.run(os.getenv("BOT_TOKEN"))
