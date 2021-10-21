0e7ae6c7f9e88be9966b95f1689034f79b95d4b1
Drawman2 <drawmant@gmail.com>
2021-10-21 16:49:49 +0200

    Added bot running

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import discord
 from discord.ext import commands
+
+import os
+
+client = commands.Bot(command_prefix = "/")
+
+#This is a decorator. It calls the following function within itself in
+#order to execute code before and after.
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(name="/help"))
+    print("Bot online and running...")
+
+client.run(os.get_dotenv(BOT_TOKEN))
